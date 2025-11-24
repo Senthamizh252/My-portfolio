7238947d16f734ceeec635452e6bc814e99babd3
Senthamizh Senthilvel <senthamizh252@gmail.com>
2025-11-24 22:47:31 +0530

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,6 +17,11 @@ const ProjectCard = ({ project, index }) => {
         >
             <h3>{project.title}</h3>
             <p>{project.description}</p>
+            <div className="tech-stack">
+                {project.technologies && project.technologies.map((tech, i) => (
+                    <span key={i} className="tech-tag">{tech}</span>
+                ))}
+            </div>
             <button className="btn-download">{project.buttonText}</button>
         </Link>
     );
