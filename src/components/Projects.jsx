bffdb29aa8573a693b545a95f61ab945b1c3a3fe
Senthamizh Senthilvel <senthamizh252@gmail.com>
2025-11-29 15:04:39 +0530

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/style.css';
 import { projectsData } from '../data/projectsData';
+import ProjectCard from './ProjectCard';
 
 const Projects = () => {
     return (
@@ -8,20 +9,8 @@ const Projects = () => {
             <div className="container">
                 <h2 className="section-title">PROJECTS</h2>
                 <div className="projects-grid">
-                    {projectsData.map((project) => (
-                        <div key={project.id} className="project-card">
-                            {project.logo && (
-                                <img src={project.logo} alt={`${project.title} Logo`} className="project-logo" />
-                            )}
-                            <h3 className="project-title">{project.title}</h3>
-                            <p className="project-description">{project.description}</p>
-                            {project.githubLink && (
-                                <a href={project.githubLink} className="btn-github" target="_blank" rel="noopener noreferrer">GitHub</a>
-                            )}
-                            {project.driveLink && (
-                                <a href={project.driveLink} className="btn-github" target="_blank" rel="noopener noreferrer">Drive</a>
-                            )}
-                        </div>
+                    {projectsData.map(project => (
+                        <ProjectCard key={project.id} project={project} />
                     ))}
                 </div>
             </div>
